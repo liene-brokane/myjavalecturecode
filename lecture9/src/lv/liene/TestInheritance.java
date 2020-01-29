57a7e3a7eba0b24600c4bfc7026d45ab4a90d267
liene-brokane <liene.brokane@gmail.com>
2020-01-29 21:26:57 +0200

    lecture9

---
+++ lecture9/src/lv/liene/TestInheritance.java
@@ -0,0 +1,36 @@
+package lv.liene;
+
+import lv.liene.graph.Point2D;
+import lv.liene.graph.Point3D;
+import lv.liene.graph.Point4D;
+
+public class TestInheritance {
+
+	public static void main(String[] args) {
+
+		Point2D point = new Point2D();
+		System.out.println(point.toString());
+		Point2D pointXY = new Point2D(10, -2);
+		System.out.println(pointXY.toString());
+
+		pointXY.setX(8);
+		pointXY.setY(-6);
+
+		System.out.println(pointXY);
+
+		System.out.println("x is: " + pointXY.getX());
+
+		Point3D pointXYZ = new Point3D(3, 1, 10);
+		System.out.println(pointXYZ);
+		
+		pointXYZ.setY(-99);
+		System.out.println(pointXYZ);
+		
+		Point4D pointXYZT = new Point4D(3, 4, 2, 2);
+		System.out.println(pointXYZT);
+		
+		System.out.println(pointXYZT.getX());
+
+	}
+
+}
---
+++ lecture9/src/lv/liene/users/Teacher.java
@@ -0,0 +1,38 @@
+package lv.liene.users;
+
+import java.util.Arrays;
+
+public class Teacher extends Person {
+
+	private int numCourses;
+	private String[] courses;
+	private static final int MAX_COURSES = 4;
+
+	public Teacher(String name, String address) {
+		super(name, address);
+		this.numCourses = 0;
+		this.courses = new String[MAX_COURSES];
+
+	}
+
+	public void addCourse(String course) {
+
+		courses[numCourses] = course;
+		++numCourses;
+//		System.out.println(Arrays.toString(this.courses));
+
+	}
+
+	public boolean addCourses(String course) {
+		return true;
+	}
+
+	public boolean removeCourses(String course) {
+		return true;
+	}
+
+	public String toString() {
+		return "Teacher: " + super.toString();
+	}
+
+}
